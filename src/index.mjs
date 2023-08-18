e09517d2fd23b46c5af6d538a4ad2ead0308f890
YutaKoike262 <142475854+YutaKoike262@users.noreply.github.com>
2023-08-18 11:56:59 +0900

    Update index.mjs
    コメントアウトを削除

--- src/index.mjs
+++ src/index.mjs
@@ -4,55 +4,7 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  /*
-  const div = document.createElement("div");
-  div.className = "list-row"
-
-  // list タグの生成
-  const li = document.createElement("li");
-  li.innerText = inputText;
-  const buttonComp = document.createElement("button")
-  buttonComp.innerText = "完了"
-  buttonComp.addEventListener("click", () => {
-    // 押された削除ボタンの親タグを削除する
-    deleteParent(buttonComp.parentNode)
-    const addTarget = buttonComp.parentNode
-    // todo 内容を取得
-    const text = addTarget.firstElementChild.innerText;
-    
-    addTarget.textContent = null;
-    // list タグの生成
-    const li = document.createElement("li");
-    li.innerText = text;
-    const buttonReturn = document.createElement("button")
-    buttonReturn.innerText = "戻す"
-    buttonReturn.addEventListener("click", () => {
-      deleteParent(buttonReturn.parentNode, "ul-completed")
-      const text = buttonReturn.parentNode.firstElementChild.innerText;
-
-    })
-    addTarget.appendChild(li)
-    addTarget.appendChild(buttonReturn)
-    document.getElementById("ul-completed").appendChild(addTarget)
-
-
-  })
-  const buttonDelete = document.createElement("button")
-  buttonDelete.innerText = "削除"
-  buttonDelete.addEventListener("click", () => {
-    // 押された削除ボタンの親タグを削除する
-    deleteParent(buttonDelete.parentNode);
-  })
-  // divの子要素に各要素を設定
-  div.appendChild(li);
-  div.appendChild(buttonComp);
-  div.appendChild(buttonDelete);
-  // 未完成の要素に追加
-  if (inputText){
-    document.getElementById("ul-incomplete").appendChild(div)
-  }
-  */
- createIncompleteList(inputText)
+  createIncompleteList(inputText)
 }
 
 document.getElementById("add-button").addEventListener("click",
